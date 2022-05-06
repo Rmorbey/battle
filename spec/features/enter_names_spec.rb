ba8ce219b9c7295c7f71a2b9eb9e3b5f2e090a76
Anish <anishkakaiya@outlook.com>
2022-05-06 15:47:29 +0100

    add player2 hit points

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -10,8 +10,16 @@ feature 'Enter names' do
     fill_in :player_2_name, with: "Russell"
     click_button 'Submit'
 
-    save_and_open_page
+    # save_and_open_page
     
     expect(page).to have_content "George vs. Russell"
+  end
+
+  scenario 'See player 2s hit points' do
+    visit ("/")
+    fill_in :player_1_name, with: "Anish"
+    fill_in :player_2_name, with: "Russell"
+    click_button 'Submit'
+    expect(page).to have_content "Russell: 60/60 HP"
   end
 end
