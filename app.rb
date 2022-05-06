52f6c0189b61d85e60f4954ab2d3ad9cefaa2027
Russell <russellmorbey@hotmail.co.uk>
2022-05-06 15:28:13 +0100

    Challenge 20# POST/redirect/GET pattern complete.

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ class Battle < Sinatra::Base
     register Sinatra::Reloader
   end
 
-  # our routes would go here
+  enable :sessions
 
   get '/' do 
     "Testing infrastructure working!"
@@ -14,8 +14,14 @@ class Battle < Sinatra::Base
   end 
 
   post '/names' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
+   session[:player_1_name] = params[:player_1_name]
+   session[:player_2_name] = params[:player_2_name]
+   redirect '/play'
+  end
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
     erb :play
   end
 
